abad400a8797a0ab5df1c72bd639f368afd1c1dd
Martin Alexanders Lopez Mamani <lopezalexanders@gmail.com>
2025-12-21 14:20:38 -0400

    modulos 2 commit

--- backend/src/clientes/clientes.module.ts
+++ backend/src/clientes/clientes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientesController } from './clientes.controller';
 import { ClientesService } from './clientes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Clientes } from './clientes';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Clientes])],
   controllers: [ClientesController],
-  providers: [ClientesService]
+  providers: [ClientesService],
 })
 export class ClientesModule {}
